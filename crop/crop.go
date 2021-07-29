6327711da347c62c714ffdc38340cdb91c49bd05
thisisaaronland <aaron@localhost>
2021-07-29 11:45:31 -0700

    snapshot: code to use geom/planar/clip compiles; doesn't seem to work...

--- crop/crop.go
+++ crop/crop.go
@@ -4,12 +4,14 @@ package crop
 import (
 	"context"
 	"fmt"
-	_ "github.com/go-spatial/geom/slippy"
+	"github.com/go-spatial/geom"
+	"github.com/go-spatial/geom/planar/clip"
 	"github.com/paulmach/orb"
-	"github.com/paulmach/orb/clip"
+	// "github.com/paulmach/orb/clip"
 	"github.com/paulmach/orb/geojson"
 	"github.com/paulmach/orb/maptile"
-	"log"
+	"github.com/sfomuseum/go-whosonfirst-tiles/geometry"
+	_ "log"
 )
 
 // CropFeatureWithTile will crop the geometry of a GeoJSON Feature defined by 'body' to the extent of 'tile'.
@@ -28,17 +30,49 @@ func CropFeatureWithBounds(ctx context.Context, body []byte, bounds orb.Bound) (
 		return nil, fmt.Errorf("Failed to unmarshal feature, %w", err)
 	}
 
-	geom := f.Geometry
+	geom_geometry, err := geometry.OrbToGeom(ctx, f.Geometry)
 
-	clipped_geom := clip.Geometry(bounds, geom)
-
-	if clipped_geom == nil {
-		log.Println("CROP", bounds.Min.Lon(), bounds.Min.Lat(), bounds.Max.Lon(), bounds.Max.Lat())
-		log.Println("FROM", geom.Bound())
-
-		return nil, fmt.Errorf("Failed to derive clipped geometry")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to convert orb geometry to geom geometry, %w", err)
 	}
 
-	f.Geometry = clipped_geom
+	geom_bounds, err := geometry.OrbToGeom(ctx, bounds.ToPolygon())
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to convert orb bounds to geom geometry, %w", err)
+	}
+
+	geom_extent, err := geom.NewExtentFromGeometry(geom_bounds)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create extent from bounds, %w", err)
+	}
+
+	geom_cropped, err := clip.Geometry(ctx, geom_geometry, geom_extent)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to crop geometry, %w", err)
+	}
+
+	orb_cropped, err := geometry.GeomToOrb(ctx, geom_cropped)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to convert geom geometry to orb geometry, %w", err)
+	}
+
+	/*
+
+		clipped_geom := clip.Geometry(bounds, geom)
+
+		if clipped_geom == nil {
+			log.Println("CROP", bounds.Min.Lon(), bounds.Min.Lat(), bounds.Max.Lon(), bounds.Max.Lat())
+			log.Println("FROM", geom.Bound())
+			return nil, fmt.Errorf("Failed to derive clipped geometry")
+		}
+
+		f.Geometry = clipped_geom
+	*/
+
+	f.Geometry = orb_cropped
 	return f.MarshalJSON()
 }
